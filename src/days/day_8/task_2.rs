98ebd14cbef0a310e11532068a2eddb4abd43a64
Ratnesh <ratneshjain40@gmail.com>
2023-12-15 15:08:25 +0530

    Refactor data structure and traversal logic

--- src/days/day_8/task_2.rs
+++ src/days/day_8/task_2.rs
@@ -1,19 +1,81 @@
-use std::{cmp::Ordering, fs::read_to_string};
+use std::{collections::HashMap, fs::read_to_string};
 
-#[derive(Debug, Clone)]
-struct Hand {
-    pub hand_type: u16,
-    pub hand_value: usize,
-    pub bid: usize,
+struct Data {
+    pub map: HashMap<String, (String, String)>,
+    pub loops: HashMap<String, Vec<String>>,
 }
 
-impl Hand {
-    pub fn compare(&self, other: &Hand) -> Ordering {
-        let cp = self.hand_type.cmp(&other.hand_type);
-        if cp == Ordering::Equal {
-            return self.hand_value.cmp(&other.hand_value);
+impl Data {
+    fn find_all_loops(&mut self, start: &str, current: &str, directions: &mut String) {
+        if directions.len() > 0 && current == start {
+            self.loops
+                .entry(start.to_string())
+                .or_insert(Vec::new())
+                .push(directions.clone());
+            return;
+        }
+        if current == "ZZZ" {
+            directions.pop();
+            return;
+        }
+
+        let left = self.traverse(current.to_string(), 'L');
+
+        if left != current && left != "ZZZ" {
+            directions.push('L');
+            self.find_all_loops(start, &left, directions);
+            directions.pop();
+        }
+
+        let right = self.traverse(current.to_string(), 'R');
+        if right != current && right != "ZZZ" {
+            directions.push('R');
+            self.find_all_loops(start, &right, directions);
+            directions.pop();
+        }
+    }
+
+    pub fn find_path_to_end(&self, current: &str, directions: &mut String) {
+        if current == "ZZZ" {
+            return;
+        }
+
+        let left = self.traverse(current.to_string(), 'L');
+        if left != current && left != "ZZZ" {
+            directions.push('L');
+            self.find_path_to_end(&left, directions);
+        }
+
+        let right = self.traverse(current.to_string(), 'R');
+        if right != current && right != "ZZZ" {
+            directions.push('R');
+            self.find_path_to_end(&right, directions);
+        }
+    }
+}
+
+impl Data {
+    pub fn new() -> Self {
+        Self {
+            map: HashMap::new(),
+            loops: HashMap::new(),
+        }
+    }
+
+    pub fn get(&self, key: String) -> (String, String) {
+        self.map.get(&key).unwrap().clone()
+    }
+
+    pub fn insert(&mut self, key: String, value: (String, String)) {
+        self.map.insert(key, value);
+    }
+
+    pub fn traverse(&self, from: String, direction: char) -> String {
+        let peers = self.get(from);
+        if direction == 'L' {
+            peers.0
         } else {
-            return cp;
+            peers.1
         }
     }
 }
@@ -26,100 +88,36 @@ fn read_input(filepath: &str) -> Vec<String> {
         .collect()
 }
 
-fn calculate_hand_type(hand_string: Vec<char>) -> u16 {
-    let hand_vec: Vec<char> = hand_string.to_vec();
+fn parse_input(mut lines: Vec<String>) -> (String, Data) {
+    let path = lines.remove(0);
+    lines.remove(0);
 
-    let mut char_counts = std::collections::HashMap::new();
-    for &ch in &hand_vec {
-        *char_counts.entry(ch).or_insert(0) += 1;
-    }
-
-    let number_of_jokers = char_counts.get(&'J').unwrap_or(&0).clone();
-    char_counts.remove(&'J');
-
-    let mut values = char_counts.values().collect::<Vec<&usize>>().into_iter().map(|x| *x).collect::<Vec<usize>>();
-    values.sort();
-    let mut values = values.into_iter().rev().collect::<Vec<usize>>();
-    
-    if let Some(first) = values.first_mut() {
-        *first = *first + number_of_jokers;
-    } else {
-        values.push(number_of_jokers);
-    }
-
-    if values[0] == 5 {
-        return 7;
-    } else if values[0] == 4 {
-        return 6;
-    } else if values[0] == 3 {
-        if values[1] == 2 {
-            return 5;
-        } else {
-            return 4;
-        }
-    } else if values[0] == 2 {
-        if values[1] == 2 {
-            return 3;
-        } else {
-            return 2;
-        }
-    } else {
-        return 1;
-    }
-}
-fn parse_input(lines: Vec<String>) -> Vec<Hand> {
-    let symbol_mapping: std::collections::HashMap<char, char> = [
-        ('J', '1'),
-        ('2', '2'),
-        ('3', '3'),
-        ('4', '4'),
-        ('5', '5'),
-        ('6', '6'),
-        ('7', '7'),
-        ('8', '8'),
-        ('9', '9'),
-        ('T', 'A'),
-        ('Q', 'B'),
-        ('K', 'C'),
-        ('A', 'D'),
-    ]
-    .iter()
-    .cloned()
-    .collect();
-
-    let mut hands: Vec<Hand> = Vec::new();
+    let mut map = Data::new();
     for line in lines {
-        let (cards, bid) = line.split_once(" ").unwrap();
-        let bid = bid.parse::<usize>().unwrap();
-        let crad_transformed = cards
-            .chars()
-            .map(|x| symbol_mapping.get(&x).unwrap())
-            .collect::<String>();
-        let hex = usize::from_str_radix(&crad_transformed, 16).unwrap();
-        let hand_type = calculate_hand_type(cards.chars().collect::<Vec<char>>());
-        let hand_value = hex as usize;
-        hands.push(Hand {
-            hand_type,
-            hand_value,
-            bid,
-        });
+        let (raw_node, raw_peers) = line.split_once("=").unwrap();
+        let node = raw_node.trim();
+        let (raw_left, raw_right) = raw_peers.trim().split_once(", ").unwrap();
+        let left = raw_left.trim().strip_prefix("(").unwrap();
+        let right = raw_right.trim().strip_suffix(")").unwrap();
+        map.insert(node.to_string(), (left.to_string(), right.to_string()));
     }
-    return hands;
+    return (path, map);
 }
 
 pub fn run(filename: &str) -> usize {
-    let lines = read_input(format!("src/days/day_7/{}", filename).as_str());
-    let mut hands = parse_input(lines);
-    hands.sort_by(|a, b| a.compare(b));
+    let lines = read_input(format!("src/days/day_8/{}", filename).as_str());
+    let (path, mut map) = parse_input(lines);
 
-    let winnings: Vec<usize> = hands
-        .iter()
-        .enumerate()
-        .map(|(i, hand)| {
-            return hand.bid * (i + 1);
-        }).collect();
-    
-    return winnings.iter().sum();
+    let keys = map.map.keys().cloned().collect::<Vec<String>>();
+    for key in keys {
+        println!("For {}", key);
+        map.find_all_loops(&key, &key, &mut String::new());
+    }
+    println!("{:?}", map.loops);
+
+
+    println!("Final Directions");
+    todo!()
 }
 
 #[cfg(test)]
@@ -128,12 +126,6 @@ mod tests {
 
     #[test]
     fn test_run() {
-        assert_eq!(run("input_test.txt"), 5905);
-    }
-
-    #[test]
-    fn main_test() {
-        assert_eq!(run("input.txt"), 251121738);
-
+        assert_eq!(run("input_test_2.txt"), 2);
     }
 }
